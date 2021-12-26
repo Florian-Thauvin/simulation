7205a8c99af3e9202677afa27e3ff2f8f90c5b29
Florian Thauvin <florian.thauvin@orange.fr>
2021-12-26 15:37:40 +0100

    Mise en place du client

---
+++ client/src/ui/MainPage.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Header } from "./widgets/Header";
+
+export function MainPage(): JSX.Element {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
---
+++ client/src/ui/widgets/Header.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import "./header.scss";
+
+export function Header(): JSX.Element {
+  return <h1>Simulation</h1>;
+}

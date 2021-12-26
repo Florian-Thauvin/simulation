fbf316e0d578e7b9604f595d57c553d0c530baf4
Florian Thauvin <florian.thauvin@orange.fr>
2021-12-26 09:46:03 +0100

    Mep client

--- client/src/index.ts
+++
@@ -1,7 +0,0 @@
-function run(): void {
-    const mainPage = document.createElement('div');
-}
-
-run();
-
-export{};
---
+++ client/src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <div>
+        <p>Titit</p>
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+reportWebVitals(console.log);

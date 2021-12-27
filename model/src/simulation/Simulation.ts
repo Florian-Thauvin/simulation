abdafed251c8fe9f3ccb2c8f8b9ee11464980488
Florian Thauvin <florian.thauvin@orange.fr>
2021-12-27 07:31:04 +0100

    Ajout du model 2d

---
+++ model/src/simulation/Simulation.ts
@@ -0,0 +1,9 @@
+import { Person } from "../vector/Person";
+
+export class Simulation {
+  public population: Person[];
+
+  constructor() {
+    this.population = [];
+  }
+}
--- model/src/vector/Person.ts
+++ model/src/vector/Person.ts
@@ -1,5 +1,6 @@
 import { AbstractData } from "../AbstractData";
 import { Point } from "../geometry/Point";
+import { Vector } from "../geometry/Vector";
 import { EState } from "./State";
 
 export class Person extends AbstractData {
@@ -7,10 +8,13 @@ export class Person extends AbstractData {
 
   public position: Point;
 
+  public vector: Vector;
+
   constructor() {
     super();
 
     this.state = EState.CLEAN;
     this.position = new Point();
+    this.vector = new Vector();
   }
 }
